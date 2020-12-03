4cb5fd2cfd14e6f79a84c21b3cd0abfb2b300f54
Cyber Oliveira <lccro@msn.com>
2020-12-03 18:36:29 -0300

    day 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib where
 
 import Data.List.Split
+import Debug.Trace
 
 ------------------------------------------------------------------------ 01 --
 d01_1 :: IO Int
@@ -39,7 +40,30 @@ d02_2 = length . filter correct . map words . lines <$> readFile "src/02-1.txt"
 
 ------------------------------------------------------------------------ 03 --
 d03_1 :: IO Int
-d03_1 = return 3
+d03_1 = go 3 0 . drop 1 . lines <$> readFile "src/03-1.txt"
+  where
+    go i acc (x : xs)
+      -- | trace ("go " ++ show i ++ " " ++ show x) False = undefined
+      | null xs = acc + xo
+      | otherwise = go (rem (i + 3) (length x)) (acc + xo) xs
+      where xo = if (x !! i) == '#' then 1 else 0
+
+d03_2 :: IO Int
+d03_2 = p2 . lines <$> readFile "src/03-1.txt"
+  where
+    p2 lst = product [(go1 1 1 0 t),(go1 3 3 0 t),(go1 5 5 0 t),(go1 7 7 0 t),(go2 1 1 0 tt)]
+      where t = drop 1 lst
+            tt = drop 2 lst
+    go1 inc i acc (x : xs)
+      -- | trace ("go1 " ++ show i ++ " " ++ show acc ++ " " ++ show xo) False = undefined
+      | null xs = acc + xo
+      | otherwise = go1 inc (rem (i + inc) (length x)) (acc + xo) xs
+      where xo = if (x !! i) == '#' then 1 else 0
+    go2 inc i acc (x : xs)
+      -- | trace ("go2 " ++ show i ++ " " ++ show acc ++ " " ++ show xo) False = undefined
+      | null xs = acc + xo
+      | otherwise = go2 inc (rem (i + inc) (length x)) (acc + xo) (tail xs)
+      where xo = if (x !! i) == '#' then 1 else 0
 
 someFunc :: IO ()
-someFunc = fmap show d03_1 >>= putStrLn
+someFunc = fmap show d03_2 >>= putStrLn
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -14,3 +14,7 @@ spec = do
     it "day 02" $ do
       d02_1 `shouldReturn` 445
       d02_2 `shouldReturn` 491
+
+    it "day 03" $ do
+      d03_1 `shouldReturn` 198
+      d03_2 `shouldReturn` 5140884672
