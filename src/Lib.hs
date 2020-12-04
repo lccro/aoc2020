7a9a6085197499dfd1812cd20a68a2082862742f
Cyber Oliveira <lccro@msn.com>
2020-12-04 13:32:17 -0300

    simplifies day 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -40,30 +40,29 @@ d02_2 = length . filter correct . map words . lines <$> readFile "src/02-1.txt"
 
 ------------------------------------------------------------------------ 03 --
 d03_1 :: IO Int
-d03_1 = go 3 0 . drop 1 . lines <$> readFile "src/03-1.txt"
+d03_1 = foldr go 0 . zip [3,6..] . tail . (map . map) xo . lines <$> readFile "src/03-1.txt"
   where
-    go i acc (x : xs)
-      -- | trace ("go " ++ show i ++ " " ++ show x) False = undefined
-      | null xs = acc + xo
-      | otherwise = go (rem (i + 3) (length x)) (acc + xo) xs
-      where xo = if (x !! i) == '#' then 1 else 0
+    xo '#' = 1
+    xo _ = 0
+    go (ndx, lst) acc = acc + (lst !! (ndx `rem` (length lst)))
 
 d03_2 :: IO Int
-d03_2 = p2 . lines <$> readFile "src/03-1.txt"
+d03_2 = p2 . (map . map) xo . lines <$> readFile "src/03-1.txt"
   where
-    p2 lst = product [(go1 1 1 0 t),(go1 3 3 0 t),(go1 5 5 0 t),(go1 7 7 0 t),(go2 1 1 0 tt)]
-      where t = drop 1 lst
-            tt = drop 2 lst
-    go1 inc i acc (x : xs)
-      -- | trace ("go1 " ++ show i ++ " " ++ show acc ++ " " ++ show xo) False = undefined
-      | null xs = acc + xo
-      | otherwise = go1 inc (rem (i + inc) (length x)) (acc + xo) xs
-      where xo = if (x !! i) == '#' then 1 else 0
-    go2 inc i acc (x : xs)
-      -- | trace ("go2 " ++ show i ++ " " ++ show acc ++ " " ++ show xo) False = undefined
-      | null xs = acc + xo
-      | otherwise = go2 inc (rem (i + inc) (length x)) (acc + xo) (tail xs)
-      where xo = if (x !! i) == '#' then 1 else 0
+    xo '#' = 1
+    xo _ = 0
+
+    p2 lst = product [
+      (foldr go 0 . zip [1,2..] $ t),
+      (foldr go 0 . zip [3,6..] $ t),
+      (foldr go 0 . zip [5,10..] $ t),
+      (foldr go 0 . zip [7,14..] $ t),
+      (foldr go 0 . zip [1,2..] $ tt . tail $ lst)
+      ]
+      where t = tail lst
+            tt (_:y:xs) = y : tt xs
+            tt _ = []
+    go (ndx, lst) acc = acc + (lst !! (ndx `rem` (length lst)))
 
 someFunc :: IO ()
 someFunc = fmap show d03_2 >>= putStrLn
